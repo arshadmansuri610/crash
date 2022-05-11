3425a6ef02663b263502ef05a0b8ac5b39d5698d
Arshad Mansuri <arshadmansuri610@gmail.com>
2022-05-12 01:24:40 +0530

    till for

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // strings, numbers, boolean, null, undefined
 
-const name = "John";
+/* const name = "John";
 const age = 30;
 const rating = 4.5;
 const iscool = true;
@@ -14,4 +14,35 @@ console.log(typeof rating);
 console.log(typeof iscool);
 console.log(typeof x);
 console.log(typeof y);
-console.log(typeof z);
+console.log(typeof z); */
+
+const todos = [
+    {
+        id: 1,
+        text: 'Take out Trash',
+        isCompleted: true
+    },
+    {
+        id: 2,
+        text: 'Meeting with boss',
+        isCompleted: true
+    },
+    {
+        id: 3,
+        text: 'Dentist appt',
+        isCompleted: false
+    },
+]
+//console.log(todos[1].text)
+
+//const toJSON = JSON.stringify(todos);
+//console.log(toJSON);
+
+/*for(let i = 0; i < 10; i++) {
+    console.log(`to add label: ${i}`);
+}*/
+
+/*for(let i = 0; i < 10; i++){
+    console.log(i)
+}*/
+
