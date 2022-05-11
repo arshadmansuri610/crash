cb430f37706a15c6e1ac801d23da6a685c9d950f
Arshad Mansuri <arshadmansuri610@gmail.com>
2022-05-12 00:47:16 +0530

    types

---
+++ main.js
@@ -0,0 +1,17 @@
+// strings, numbers, boolean, null, undefined
+
+const name = "John";
+const age = 30;
+const rating = 4.5;
+const iscool = true;
+const x = null;
+const y = undefined;
+let z;
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof rating);
+console.log(typeof iscool);
+console.log(typeof x);
+console.log(typeof y);
+console.log(typeof z);
