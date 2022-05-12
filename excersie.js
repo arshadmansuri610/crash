576809e7b14abc357f1d1cf2d36bb1c3dc6acac7
Arshad Mansuri <arshadmansuri610@gmail.com>
2022-05-12 13:56:29 +0530

    star code working

--- excersie.js
+++ excersie.js
@@ -1,10 +1,22 @@
-const input = 10;
+const input = 12;
 
 // let star = '*'
 // console.log(star);
 
 p = 0
-while(p <= input){
-    console.log('*')
+while(p <= (input-1)){
+    
+    
+    // console.log('*')
+    j = 0;
+    let stars = '*';
+    while(j <= (p-1)) {
+        // console.log(j + ' *');
+        stars = stars + '*';
+        j = j + 1;
+    }    
+    
+    console.log(stars)
+    
     p = p + 1
 }
